17616b72fe0691d46750198acc059ebb9c279fdd
Young <gooker_young@163.com>
2024-11-11 21:50:23 +0800

    add cli

---
+++ src/fspacker/config.py
@@ -0,0 +1,7 @@
+import typing
+
+# python 镜像
+EMBED_URL_PREFIX: typing.Dict[str, str] = dict(
+    official="https://www.python.org/ftp/python/",
+    huawei="https://mirrors.huaweicloud.com/python/",
+)
