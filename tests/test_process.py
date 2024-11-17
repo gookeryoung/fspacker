e08287fd3fa532030983e4b9f0b8bbf294404afa
Young <gooker_young@163.com>
2024-11-17 10:58:10 +0800

    update tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,11 +1,20 @@
+import gc
+
 import pytest
 
 from fspacker.process import Processor
 from tests.common import DIR_EXAMPLES, exec_dist_dir
 
 
+@pytest.fixture(autouse=True)
+def disable_gc():
+    gc.disable()
+    yield
+    gc.enable()
+
+
 @pytest.mark.benchmark(group="console")
-def test_pack_ex01():
+def test_pack_ex01(disable_gc):
     root_dir = DIR_EXAMPLES / "ex01_helloworld_console"
     proc = Processor(root_dir)
     proc.run()
@@ -14,7 +23,7 @@ def test_pack_ex01():
 
 
 @pytest.mark.benchmark(group="tkinter")
-def test_pack_ex02():
+def test_pack_ex02(disable_gc):
     root_dir = DIR_EXAMPLES / "ex02_hello_gui"
     proc = Processor(root_dir)
     proc.run()
@@ -23,7 +32,7 @@ def test_pack_ex02():
 
 
 @pytest.mark.benchmark(group="pyside2")
-def test_pack_ex03():
+def test_pack_ex03(disable_gc):
     root_dir = DIR_EXAMPLES / "ex03_pyside2_simple"
     proc = Processor(root_dir)
     proc.run()
@@ -32,7 +41,7 @@ def test_pack_ex03():
 
 
 @pytest.mark.benchmark(group="pyside2")
-def test_pack_ex04():
+def test_pack_ex04(disable_gc):
     root_dir = DIR_EXAMPLES / "ex04_pyside_complex"
     proc = Processor(root_dir)
     proc.run()
@@ -40,7 +49,7 @@ def test_pack_ex04():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex05():
+def test_pack_ex05(disable_gc):
     root_dir = DIR_EXAMPLES / "ex05_tkinter"
     proc = Processor(root_dir)
     proc.run()
@@ -48,7 +57,7 @@ def test_pack_ex05():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex12():
+def test_pack_ex12(disable_gc):
     root_dir = DIR_EXAMPLES / "ex12_web_bottle"
     proc = Processor(root_dir)
     proc.run()
@@ -56,7 +65,7 @@ def test_pack_ex12():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex13():
+def test_pack_ex13(disable_gc):
     root_dir = DIR_EXAMPLES / "ex13_pypdf"
     proc = Processor(root_dir)
     proc.run()
@@ -64,7 +73,7 @@ def test_pack_ex13():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex20():
+def test_pack_ex20(disable_gc):
     root_dir = DIR_EXAMPLES / "ex20_pygame_snake"
     proc = Processor(root_dir)
     proc.run()
@@ -72,7 +81,7 @@ def test_pack_ex20():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex22():
+def test_pack_ex22(disable_gc):
     root_dir = DIR_EXAMPLES / "ex22_matplotlib"
     proc = Processor(root_dir)
     proc.run()
@@ -80,7 +89,7 @@ def test_pack_ex22():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex23():
+def test_pack_ex23(disable_gc):
     root_dir = DIR_EXAMPLES / "ex23_numba"
     proc = Processor(root_dir)
     proc.run()
@@ -88,7 +97,7 @@ def test_pack_ex23():
     assert exec_dist_dir(root_dir / "dist")
 
 
-def test_pack_ex24():
+def test_pack_ex24(disable_gc):
     root_dir = DIR_EXAMPLES / "ex24_pandas"
     proc = Processor(root_dir)
     proc.run()
