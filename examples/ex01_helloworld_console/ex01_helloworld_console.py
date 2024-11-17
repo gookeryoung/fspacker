684a81dfb2dbc741545ac0aa3d92e1d4fbc591d6
Young <gooker_young@163.com>
2024-11-17 18:16:16 +0800

    udpate ex01, add multi modules

--- examples/ex01_helloworld_console/ex01_helloworld_console.py
+++ examples/ex01_helloworld_console/ex01_helloworld_console.py
@@ -1,2 +1,11 @@
+from module_c import function_c
+from modules.module_a import function_a
+from modules.module_b import function_b
+
+
 def main():
     print("hello, world")
+
+    function_a()
+    function_b()
+    function_c()
