e180502ddbb0eb9bd65025401578c3b42576ff89
Young <gooker_young@163.com>
2024-11-19 22:19:36 +0800

    fix import error

--- fspacker/parser/source.py
+++ fspacker/parser/source.py
@@ -2,6 +2,7 @@ import ast
 import logging
 import pathlib
 import typing
+from io import StringIO
 
 from fspacker.common import PackTarget
 from fspacker.config import TKINTER_LIBS
@@ -18,13 +19,13 @@ class SourceParser(BaseParser):
         with open(entry, encoding="utf-8") as f:
             code = "".join(f.readlines())
             if "def main" in code or "__main__" in code:
-                ast_tree, extra, deps = self._parse_ast(code, entry)
+                ast_tree, extra, deps, text = self._parse_ast(code, entry)
                 self.config.targets[entry.stem] = PackTarget(
                     src=entry,
                     deps=deps,
                     ast=ast_tree,
                     extra=extra,
-                    code=code,
+                    code=f"{code}{text}",
                 )
                 logging.info(f"Add pack target{self.config.targets[entry.stem]}")
 
@@ -34,32 +35,32 @@ class SourceParser(BaseParser):
 
         builtins = get_builtin_lib_repo()
         tree = ast.parse(content, filename=filepath)
-        entries = list(_.stem for _ in filepath.parent.iterdir())
+        entries: typing.Dict[str, pathlib.Path] = {_.stem: _ for _ in filepath.parent.iterdir()}
         imports = set()
         extra = set()
         deps = set()
+        code_text = StringIO()
 
         for node in ast.walk(tree):
+            import_name: typing.Optional[str] = None
+
             if isinstance(node, ast.ImportFrom):
                 if node.module is not None:
                     import_name = node.module.split(".")[0].lower()
-
-                    # import from local files or package folders
-                    if import_name in entries:
-                        deps.add(import_name)
-
-                    # imports ast
-                    elif import_name not in builtins:
-                        imports.add(import_name)
             elif isinstance(node, ast.Import):
                 for alias in node.names:
                     import_name = alias.name.split(".")[0].lower()
-                    if import_name in entries:
-                        deps.add(import_name)
-                    elif import_name not in builtins:
-                        imports.add(import_name)
 
-                    if import_name in TKINTER_LIBS:
-                        extra.add("tkinter")
+            if import_name is not None:
+                # import from local files or package folders
+                if import_name in entries:
+                    deps.add(import_name)
+                    with open(entries[import_name], encoding="utf-8") as f:
+                        code_text.write("".join(f.readlines()))
+                elif import_name not in builtins:
+                    imports.add(import_name)
 
-        return imports, extra, deps
+                if import_name in TKINTER_LIBS:
+                    extra.add("tkinter")
+
+        return imports, extra, deps, code_text.getvalue()
