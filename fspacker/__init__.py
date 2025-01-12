f2743e9199c0a38a9c760ae49ff967195cd5930d
gooker_young <gooker_young@qq.com>
2025-01-12 23:36:22 +0800

    add dynamic version support

--- fspacker/__init__.py
+++ fspacker/__init__.py
@@ -1,3 +1,3 @@
- These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0.post252.dev0+8931671"
-__version_tuple__ = (0, 0, 0, "post252", "dev0", 8931671)
+# These version placeholders will be replaced later during substitution.
+__version__ = "0.6.1"
+__version_tuple__ = (0, 6, 1)
