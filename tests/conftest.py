0287b04dfde3a24b819e2fafd8d8852467164d86
gooker_young <gooker_young@qq.com>
2025-02-14 21:24:42 +0800

    ~pre-commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -68,7 +68,7 @@ def _run_project(project_dir: pathlib.Path, timeout: int = TEST_CALL_TIMEOUT):
 
 @pytest.fixture(autouse=True, scope="session")
 def clear_dist_folders():
-    print(f"Clear all dist folders.")
+    print("Clear all dist folders.")
     dist_folders = list(x for x in DIR_EXAMPLES.rglob("dist") if x.is_dir())
     for dist_folder in dist_folders:
         shutil.rmtree(dist_folder)
